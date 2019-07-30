b8cdc58897b7b1758881cf0904da9456a254b5b9
Rori <r_ori@hotmail.co.uk>
2019-07-30 12:47:23 +0100

    added the back end and added the create a new user functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: users, include: [:scores]
+  end
+
+  def create
+      user = User.create(name: params[:name])
+  end
+
+
+
+end
